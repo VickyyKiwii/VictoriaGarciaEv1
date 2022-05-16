de82147db0c190d787e7d4e6a82cdae8aa2e8b65
VictoriaAndrea <91353425+VickyyKiwii@users.noreply.github.com>
2022-05-16 14:59:05 -0400

    Add files via upload

---
+++ Ev1VictoriaGarcia/Proyecto001/Buque.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto001
+{
+    internal class Buque
+    {
+        ///Atributos clase Buque
+        private String codigo;
+        private int MinLenght = 5;
+        private String nombre;
+        private String pais;
+        private int cantidadContainers;
+        private int cantidadContainersCargados;
+        private int gastoTransporte;
+        List<Container> listaContainers;
+
+        /// <summary>
+        /// Constructor vacío
+        /// </summary>
+        public Buque()
+        {
+        }
+
+        /// <summary>
+        ///Constructor con parametros
+        /// </summary>
+        public Buque(string codigo, string nombre, string pais, int cantidadContainers, int cantidadContainersCargados, int gastoTransporte, List<Container> listaContainers)
+        {
+            this.Codigo = codigo;
+            this.Nombre = nombre;
+            this.Pais = pais;
+            this.CantidadContainers = cantidadContainers;
+            this.CantidadContainersCargados = cantidadContainersCargados;
+            this.GastoTransporte = gastoTransporte;
+            this.ListaContainers = listaContainers;
+        }
+
+        /// <summary>
+        /// Métodos Getter y Setter
+        /// </summary>
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Pais { get => pais; set => pais = value; }
+        public int CantidadContainers { get => cantidadContainers; set => cantidadContainers = value; }
+        public int CantidadContainersCargados { get => cantidadContainersCargados; set => cantidadContainersCargados = value; }
+        public int GastoTransporte { get => gastoTransporte; set => gastoTransporte = value; }
+        internal List<Container> ListaContainers { get => listaContainers; set => listaContainers = value; }
+
+        /// <summary>
+        /// Metodo ToString()
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Buque: " + Codigo + " " + Nombre + " " + Pais + " " + CantidadContainers + " " + CantidadContainersCargados + " " + GastoTransporte;
+        }
+
+        public void CantCodigo()
+        {
+
+            if (Codigo.Length < 5)
+            {
+
+                Console.WriteLine("Codigo debe ser igual o mayor a 5 caracteres");
+
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public void GastosEnvio()
+        {
+
+        }
+
+        public void SubirContainer()
+        {
+            if CantidadContainer(){
+
+            }
+            {
+
+            }
+        }
+    }
+}
---
+++ Ev1VictoriaGarcia/Proyecto001/Container.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto001
+{
+    internal class Container
+    {
+        ///Atributos clase Container
+        private String codigo;
+        private String marca;
+        private int capacidadMaxima;
+        private byte tamaño;
+        private bool esRefrigerado;
+        private int pesoActual;
+        private Buque buque;
+
+        /// <summary>
+        /// Constructor vacío
+        /// </summary>
+        public Container()
+        {
+        }
+
+        /// <summary>
+        /// Constructor con parametros
+        /// </summary>
+        public Container(string codigo, string marca, int capacidadMaxima, byte tamaño, bool esRefrigerado, int pesoActual, Buque buque)
+        {
+            this.Codigo = codigo;
+            this.Marca = marca;
+            this.CapacidadMaxima = capacidadMaxima;
+            this.Tamaño = tamaño;
+            this.EsRefrigerado = esRefrigerado;
+            this.PesoActual = pesoActual;
+            this.Buque = buque;
+        }
+
+        /// <summary>
+        /// Metodos Getter y Setter para cada atributo
+        /// </summary>
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public int CapacidadMaxima { get => capacidadMaxima; set => capacidadMaxima = value; }
+        public byte Tamaño { get => tamaño; set => tamaño = value; }
+        public bool EsRefrigerado { get => esRefrigerado; set => esRefrigerado = value; }
+        public int PesoActual { get => pesoActual; set => pesoActual = value; }
+        internal Buque Buque { get => buque; set => buque = value; }
+
+        public override string ToString()
+        {
+            return "Container: " + Codigo + " " + Marca + " " + CapacidadMaxima + " " + Tamaño + " " + EsRefrigerado + " " + PesoActual + "" + Buque;
+        }
+
+        public void ReglaPesoActual()
+        {
+            if (PesoActual > 0 && PesoActual < capacidadMaxima)
+            {
+                Console.WriteLine("El peso actual del Container es el correcto");
+            }
+            else
+            {
+                Console.WriteLine("El peso actual debe ser mayor a 0 y no puede superar la capacidad máxima de carga");
+            }
+        }
+
+        public void SacarPeso()
+        {
+            int Pesofinal;
+            Pesofinal = pesoActual - 10;
+            Console.WriteLine("El peso final del container corresponde a: " + Pesofinal);
+        }
+
+        public void ValorPagoInspeccion()
+        {
+
+        }
+
+
+        public void PuedeSubir()
+        {
+            bool permitidosubido;
+            permitidosubido = true;
+        }
+
+    }
+}
